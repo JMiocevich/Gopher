a46985cac7dd0c35442637a6f8e7d70f4628f580
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2022-10-29 10:37:54 +0800

    initial commit

---
+++ greetings/greetings.go
@@ -0,0 +1,17 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return name, errors.New("empty name")
+	}
+
+	message := fmt.Sprintf("Hello %v", name)
+	return message, nil
+}
